489d512527ba1b999426f52271bc68a894f9b852
GeorgiosVeropoulos <giwrgosv1996@gmail.com>
2022-11-28 13:46:14 +0200

    notifications work

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import SignUpPage from "./components/SignUpPage/SignUpPage.js";
 import BankAccountsContainer from "./components/BankAccountsContainer/BankAccountsContainer";
 import UserSettingsMenu from "./components/UserSettingsMenu/UserSettingsMenu";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Notifications from "./components/Notifications/Notifications";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
             <Route path="/home" exact element={<MainPage />} />
             <Route path="/myaccount" exact element={<UserSettingsMenu />} />
             <Route path="/bankaccount" exact element={<BankAccountsContainer />} />
+            <Route path="/notifications" exact element={<Notifications />} />
           </Routes>
         </Router>
       </div>
--- src/components/HeaderBar/HeaderBar.js
+++ src/components/HeaderBar/HeaderBar.js
@@ -1,8 +1,26 @@
 import "./HeaderBar.css";
 import { setDisplay } from "./../AccountSideMenu/AccountSideMenu.js";
+import { useNavigate} from 'react-router-dom';
 import * as ImIcons from 'react-icons/im';
 import * as AiIcons from 'react-icons/ai';
+
+function getCurrentURL() {
+  return window.location.href;
+  
+}
+
 function HeaderBar() {
+  const navigate = useNavigate();
+  const navigateNotifications = () =>{
+    navigate('/notifications');
+  }
+  let show = false;
+  if(getCurrentURL() === "http://localhost:3000/notifications"){
+    show = true;
+  }
+  else{
+    show = false;
+  }
   return (
     <div>
       <header>
@@ -12,15 +30,16 @@ function HeaderBar() {
           <h2>Real World App</h2>
           <div>
             <button className='new-transaction-btn'>$ NEW</button>
-            <AiIcons.AiFillBell size={25} className="notification-adjust" onClick={""}></AiIcons.AiFillBell> {/* onClick we add what needs to redirect to  */}
+            <AiIcons.AiFillBell size={25} className="notification-adjust" onClick={navigateNotifications}></AiIcons.AiFillBell> {/* onClick we add what needs to redirect to  */}
             {/* <button>Notif</button> */}
           </div>
         </div>
-        <div className="header-selection">
+        {show === true &&<div className="header-selection">
           <p>EVERYONE</p>
           <p>FRIENDS</p>
           <p>MINE</p>
-        </div>
+        </div>}
+        
       </header>
     </div>
   );
