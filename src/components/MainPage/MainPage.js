39d2106c14a802e3a4d0faa1a3b70474aa058aa0
AlexandruCotofan <alexandru0cotofan@gmail.com>
2022-11-28 10:57:42 +0100

    changes

---
+++ src/components/MainPage/MainPage.js
@@ -0,0 +1,18 @@
+import "./MainPage.css";
+import HeaderBar from "../HeaderBar/HeaderBar.js";
+import AccountSideMenu from "../AccountSideMenu/AccountSideMenu.js";
+import UserSettingsMenu from "../UserSettingsMenu/UserSettingsMenu.js";
+
+function MainPage() {
+  return (
+    <div className="main-page">
+      <AccountSideMenu />
+      <main>
+        <HeaderBar />
+        <UserSettingsMenu />
+      </main>
+    </div>
+  );
+}
+
+export default MainPage;
